cfb03f624391274ecc26c383b25a068e93ed8a86
Hasan Emre AVARİSLİ <81103889+emreavarisli@users.noreply.github.com>
2022-12-09 22:24:38 +0300

    new modelroute

--- number5_video/lib/main.dart
+++ number5_video/lib/main.dart
@@ -29,20 +29,43 @@ class _GirisEkraniState extends State<GirisEkrani> {
   TextEditingController t2 = TextEditingController();
 
   girisYap() {
-    Navigator.pushNamed(context, "/ProfilSayfasiRotasi",
+    if (t1.text == "admin" && t2.text = "1234") {
+      Navigator.pushNamed(context, "/ProfilSayfasiRotasi",
         arguments: VeriModeli(kullaniciAdi: t1.text, sifre: t2.text));
+    } else {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text("Yanlış kullanıcı adı veya şifre"),
+            content: const Text("Lütfen giriş bilgilerinizi gözden geçirin."),
+            actions: <Widget>[
+               ElevatedButton(
+                child: const Text("Kapat"),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        },
+      ); 
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Giriş Ekranı")),
-      body: Container(
-        child: Column(children: [
-          TextFormField(controller: t1),
-          TextFormField(controller: t2),
-          ElevatedButton(onPressed: girisYap, child: const Text("Giriş Yap"))
-        ]),
+      body: Center(
+        child: Container(
+          margin: const EdgeInsets.all(100),
+          child: Column(children: [
+            TextFormField(decoration: const InputDecoration(hintText: "Kullanıcı Adı"), controller: t1),
+            TextFormField(decoration: const InputDecoration(hintText: "Şifre"), controller: t2),
+            ElevatedButton(onPressed: girisYap, child: const Text("Giriş Yap"))
+          ]),
+        ),
       ),
     );
   }
