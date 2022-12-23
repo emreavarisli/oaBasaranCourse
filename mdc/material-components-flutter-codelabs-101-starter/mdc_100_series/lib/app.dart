1c88ccaaca0294999466c5c8b8a714c6229c6b21
Hasan Emre AVARİSLİ <81103889+emreavarisli@users.noreply.github.com>
2022-12-23 23:46:36 +0300

    app commit

--- mdc/material-components-flutter-codelabs-101-starter/mdc_100_series/lib/app.dart
+++ mdc/material-components-flutter-codelabs-101-starter/mdc_100_series/lib/app.dart
@@ -13,7 +13,7 @@
 // limitations under the License.
 
 import 'package:flutter/material.dart';
-
+import 'colors.dart';
 import 'home.dart';
 import 'login.dart';
 
@@ -34,10 +34,25 @@ class ShrineApp extends StatelessWidget {
         // TODO: Pass _currentCategory for frontLayer (104)
         // TODO: Change backLayer field value to CategoryMenuPage (104)
       },
-      // TODO: Add a theme (103)
+      theme: _kShrineTheme,
     );
   }
 }
 
-// TODO: Build a Shrine Theme (103)
+final ThemeData _kShrineTheme = _buildShrineTheme();
+
+ThemeData _buildShrineTheme() {
+  final ThemeData base = ThemeData.light();
+  return base.copyWith(
+    colorScheme: base.colorScheme.copyWith(
+      primary: kShrinePink100,
+      onPrimary: kShrineBrown900,
+      secondary: kShrineBrown900,
+      error: kShrineErrorRed,
+    ),
+    // TODO: Add the text themes (103)
+    // TODO: Add the icon themes (103)
+    // TODO: Decorate the inputs (103)
+  );
+}
 // TODO: Build a Shrine Text Theme (103)
